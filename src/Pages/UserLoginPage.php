3863b0171ea815aec2dbb71269412c22dbf66995
Eelke Blok <eelke@blokspeed.net>
2020-06-26 21:40:15 +0200

    Add Pages objects
    Is this the best approach? This might be different for different
    sites.

---
+++ src/Pages/UserLoginPage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Pages;
+
+/**
+ * Class UserLoginPage.
+ *
+ * @package Page
+ */
+class UserLoginPage {
+
+  const URL = '/user/login';
+  const USERLOGIN = '#user-login-form';
+
+}
---
+++ src/Pages/UserRegisterPage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pages;
+
+/**
+ * Class UserRegisterPage.
+ *
+ * @package Page
+ */
+class UserRegisterPage {
+
+  const REGISTERFORM = '#user-register-form';
+  const USERNAMEFIELD = '#edit-name';
+  const EMAILFIELD = '#edit-mail';
+
+  const LEGALAGEFIELD = '#edit-legal-profiles-0-entity-age-check-value';
+  const LEGALTERMSFIELD = '#edit-legal-profiles-0-entity-terms-check-value';
+
+}
