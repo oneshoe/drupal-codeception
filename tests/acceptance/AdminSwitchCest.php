32e1b6b4b3ee58e1e4ae14157ecf60055e84a452
Eelke Blok <eelke@blokspeed.net>
2020-06-27 11:28:30 +0200

    Add a test for switching to admin from a logged in user

--- tests/acceptance/AdminSwitchCest.php
+++ tests/acceptance/AdminSwitchCest.php
@@ -10,4 +10,16 @@ class AdminSwitchCest
     $I->switchBackFromAdmin();
     $I->assertFalse($I->amAdmin());
   }
+
+  public function switchToAdminFromVerified(AcceptanceTester $I)
+  {
+    $I->assertFalse($I->amAdmin());
+    $I->loginAs('Authenticated user');
+    $I->assertFalse($I->amAdmin());
+    $I->switchToAdmin();
+    $I->assertTrue($I->amAdmin());
+    $I->switchBackFromAdmin();
+    $I->assertFalse($I->amAdmin());
+  }
+
 }
