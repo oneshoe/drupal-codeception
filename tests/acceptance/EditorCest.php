9c089b6fa44de48ee2485444ef4098c8c5ae96f4
Eelke Blok <eelke@blokspeed.net>
2020-10-03 13:07:47 +0200

    Add a test for fillCkEditorByName
    Failing, but this way we can't forget.

--- tests/acceptance/EditorCest.php
+++ tests/acceptance/EditorCest.php
@@ -13,4 +13,17 @@ class EditorCest
     $I->click('Save');
     $I->see("Article $nodeTitle has been created.");
   }
+
+  public function filCkEditorByName(AcceptanceTester $I)
+  {
+    $I->switchToAdmin();
+    $I->amOnPage('/node/add/article');
+    $content = readfile(__DIR__ . '/../_data/basic.html');
+    $nodeTitle = $I->generateNodeTitle();
+    $I->fillField('#edit-title-0-value', $nodeTitle);
+    $I->fillCkEditorByName('edit-body-0-value', $content);
+    $I->click('Save');
+    $I->see("Article $nodeTitle has been created.");
+  }
+
 }
