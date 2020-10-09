4673882feec94687a100d1b8e87ef9dc9a2d525c
Eelke Blok <eelke@blokspeed.net>
2020-10-09 17:17:36 +0200

    Move coverage code to a trait

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -1,35 +1,17 @@
 <?php
+
 namespace Helper;
 
-use SebastianBergmann\CodeCoverage\Filter;
-use SebastianBergmann\CodeCoverage\Driver\Selector;
-use SebastianBergmann\CodeCoverage\CodeCoverage;
-use SebastianBergmann\CodeCoverage\Report\Clover;
-use SebastianBergmann\CodeCoverage\Report\Html\Facade as HtmlReport;
+class Acceptance extends \Codeception\Module {
 
-// We can not use regular CodeCeption test coverage because it is geared towards
-// coverage for the system under test. Instead wehandle coverage manually.
-class Acceptance extends \Codeception\Module
-{
-  protected $coverage;
+  use CoverageTrait;
 
   public function _beforeSuite($settings = []) {
-    $filter = new Filter;
-    $filter->includeDirectory('/app/src');
-
-    $this->coverage = new CodeCoverage(
-      (new Selector)->forLineCoverage($filter),
-      $filter
-    );
-
-    $this->coverage->start('Drupal CodeCeption');
+    $this->startCoverage();
   }
 
   public function _afterSuite() {
-    $this->coverage->stop();
-
-
-    (new HtmlReport)->process($this->coverage, '/app/tests/_output/coverage');
-    (new Clover())->process($this->coverage, '/app/tests/_output/coverage/coverage.xml');
+    $this->stopCoverage();
   }
+
 }
