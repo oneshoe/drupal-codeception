eca7e3bb6ca3fec486e7ff6f826a776248c3ea16
Eelke Blok <eelke@blokspeed.net>
2020-10-09 16:48:25 +0200

    Add a unit test run script

---
+++ scripts/ci/unit/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# To be run at the top level working directory that holds a checkout of the
+# repository named "checkout".
+
+# Stop on any error.
+set -e
+
+"$(cd -P -- "$(dirname -- "$0")" && pwd -P)/../setup-environment.sh"
+# Running the above script will have moved us to the checkout directory.
+
+cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)/../../.."
+
+echo "Run acceptance tests."
+lando codecept run unit --env=ci --xml --no-interaction --steps --debug
