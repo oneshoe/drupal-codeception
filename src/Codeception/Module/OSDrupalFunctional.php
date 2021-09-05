b26e9da236e59e72a9eac8e38f25488fed971e0c
Eelke Blok <eelke.blok@oneshoe.nl>
2021-09-05 13:07:05 +0200

    Move code from FunctionalTester to the module

--- src/Codeception/Module/OSDrupalFunctional.php
+++ src/Codeception/Module/OSDrupalFunctional.php
@@ -9,6 +9,7 @@ use Codeception\TestDrupalKernel;
 use Codeception\TestInterface;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\PhpStorage\PhpStorageFactory;
+use Exception;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -18,6 +19,13 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class OSDrupalFunctional extends Module
 {
+  /**
+   * Highwatermark for the database logging.
+   *
+   * @var int
+   */
+  protected $watchdog;
+
   /**
    * Drupal8Module constructor.
    *
@@ -82,6 +90,28 @@ class OSDrupalFunctional extends Module
       // Wipe the Twig PHP Storage cache.
       PhpStorageFactory::get('twig')->deleteAll();
     }
+
+    $this->watchdog = \Drupal::database()
+      ->query('SELECT MAX(wid) FROM {watchdog}')
+      ->fetchField();
+    \Drupal::getContainer()->get('path.alias_manager')->cacheClear();
+    $this->startTransaction();
+  }
+
+  /**
+   * Rollback transaction.
+   */
+  public function _after(TestInterface $test) {
+    $this->logWatchdog();
+    $this->rollbackTransaction();
+  }
+
+  /**
+   * Rollback transaction.
+   */
+  public function _failed(TestInterface $test, $fail) {
+    $this->logWatchdog();
+    $this->rollbackTransaction();
   }
 
   /**
@@ -103,6 +133,77 @@ class OSDrupalFunctional extends Module
   {
     if ($this->config['destroy_users']) {
       $this->tearDownTestUsers();
+    }
+  }
+
+  /**
+   * Start a drupal database transaction.
+   *
+   * @param string $name
+   *   [Optional] name for the transaction savepoint.
+   */
+  public function startTransaction($name = 'CodeceptionDrupal') {
+    $this->transaction = Database::getConnection()->startTransaction($name);
+  }
+
+  /**
+   * Rollback the drupal database transaction.
+   */
+  public function rollbackTransaction() {
+    if ($this->transaction !== NULL) {
+      try {
+        $this->transaction->rollback();
+      }
+      catch (\Exception $e) {
+        watchdog_exception('functional_tester', $e);
+      }
+
+      $this->transaction = NULL;
+    }
+  }
+
+  /**
+   * Output all the watchdog entries made during this test run.
+   *
+   * To console when ran with --debug
+   * And to a .drupallog file in the output directory.
+   */
+  protected function logWatchdog() {
+    try {
+      $query = \Drupal::database()->select('watchdog', 'w')
+        ->fields('w');
+      $query->condition('wid', $this->watchdog, '>');
+      $rows = $query->execute();
+    }
+    catch (InvalidQueryException $exception) {
+      codecept_debug($exception);
+    }
+    if (!empty($rows)) {
+      $log = '';
+      foreach ($rows as $row) {
+        $arguments = unserialize($row->variables, ['allowed_classes' => FALSE]);
+        $message = new FormattableMarkup($row->message, $arguments);
+        $message = strip_tags($message);
+        $message = html_entity_decode($message, ENT_QUOTES, 'UTF-8');
+        $levels = RfcLogLevel::getLevels();
+        /** @var \Drupal\Core\StringTranslation\TranslatableMarkup $severity */
+        $severity = $levels[$row->severity];
+        $items = [
+          date('Y-m-d\TH:i:s', $row->timestamp),
+          $severity->getUntranslatedString(),
+          $row->type,
+          $message,
+        ];
+        $log .= implode(' ', $items) . PHP_EOL;
+      }
+      if (!empty($log)) {
+        $file = codecept_output_dir() . get_class($this) . '.drupallog';
+        $content = '[[[ DRUPAL WATCHDOG ]]]' . PHP_EOL;
+        $trimmarker = PHP_EOL . '>>>>rest of string cut off. Full log: ' . $file;
+        $content .= mb_strimwidth($log, 0, 500, $trimmarker);
+        codecept_debug($content);
+        file_put_contents($file, $log, FILE_APPEND);
+      }
     }
   }
 
