5cb9b524c7071f9912947f36d411aab65388ec98
Eelke Blok <eelke.blok@oneshoe.nl>
2021-09-05 12:04:29 +0200

    Add a CI script for running functional tests

---
+++ scripts/ci/functional/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# To be run at the top level working directory that holds a checkout of the
+# repository named "checkout".
+
+source "$(dirname "$0")/../docker-common.sh"
+
+# Stop on any error.
+set -e
+
+"$(cd -P -- "$(dirname -- "$0")" && pwd -P)/../setup-environment.sh"
+
+cd checkout
+
+${dockerComposeCmd} exec -T database timeout 50 bash -c "while !(mysqladmin ping) 2>/dev/null
+do
+   sleep 3
+   echo 'Waiting for mysql ...'
+done
+"
+
+echo "Install Drupal."
+${dockerComposeCmd} exec -T php bash scripts/clean-install.sh
+
+echo "Make everything writable."
+# Especially files directory.
+sudo chown -R bamboo:bamboo .
+chmod a+w -R *
+
+echo "Set logging to verbose."
+${dockerComposeCmd} exec -T php bash -c "cd web && ../vendor/bin/drush cset system.logging error_level verbose -y"
+
+# Note: this is a dummy script that does not do anything yet, but allows us to
+# set up functional tests in Bamboo.
+exit 0
